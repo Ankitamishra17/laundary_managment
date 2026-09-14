d18a613acb5d91ad282254fe946a622f6fb869a7
Ankita Mishra <ankitamishra8763@gmail.com>
2026-09-14 18:38:23 +0530

    update

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { profileApi } from "../api/profileapi";
+import { profileApi } from "../api/profileApi";
 
 const AuthContext = createContext();
 
